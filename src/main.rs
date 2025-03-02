6f72973f8531b4b36bb564ea68ea730125af0c36
Connor Beachy <43916527+BeachyNature@users.noreply.github.com>
2025-03-02 12:41:20 -0500

    Day One Solved
    Calculate the difference of each smallest values.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use std::env;
 use std::fs;
 
 fn main() {
     
     // Read in the text input file
-    println!("Reading in the plain text file");
-    let contents = fs::read_to_string("/mnt/c/Users/tycon/Documents/RustPrograms/advent_code/inputs/inputs.txt")
-        .expect("Issues reading in text file");
+    let mut right: Vec<i32> = Vec::new();
+    let mut left: Vec<i32> = Vec::new();
 
-    // Get inputs and split them by 2 different lines
-    println!("{:?}", contents);
+    // Read in the file and split into left and right values
+    println!("Reading in the plain text file");
+    for line in fs::read_to_string("inputs.txt").unwrap().lines(){
+        let val: Vec<&str> = line.split_whitespace().collect();
+        right.push(val[0].parse().expect("Unable to parse!"));
+        left.push(val[1].parse().expect("Unable to parse!"));
+    }
+    
+    // Sort as its supposed to go in order
+    right.sort();
+    left.sort();
+
+    // Get the differences
+    let mut diff: Vec<i32> = Vec::new();
+    for idx in 0..right.len(){
+        diff.push((right[idx] - left[idx]).abs());
+    }
+
+    // Sum all of the values of the differences
+    let results: i32 = diff.iter().sum();
+    println!("{:?}", results);
 }
